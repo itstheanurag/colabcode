ba348479c34d02ba2bf5c8e7bf8d62f1a3e7ac0e
Gaurav Kumar <gauravanurag36@gmail.com>
2026-03-31 21:45:23 +0530

    initalize hono backend and install the monaco editor on react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,21 @@
+import { Editor } from "@monaco-editor/react";
+
 function App() {
-  return <div className="">Hello Suckers</div>;
+  return (
+    <main className="h-screen w-full bg-neutral-900 flex gap-4 p-4">
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+        <div className="h-full w-full">
+          <Editor
+            height="100%"
+            defaultLanguage="javascript"
+            defaultValue="// start writing code here"
+            theme="vs-dark"
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
